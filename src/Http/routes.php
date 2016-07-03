4726c9afa49d12a4ca8404d5613d3a467f992c66
Corey Mcmahon <contact@coreymcmahon.com>
2016-07-03 18:20:03 +0700

    Update throttle middleware declaration

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,14 +1,14 @@
 <?php
 
 $controller = Montopolis\MagicAuth\Http\Controllers\AuthController::class;
-// @todo: check this is working
-$throttleMiddleware = \Illuminate\Routing\Middleware\ThrottleRequests::class;
 
 $router->group([
     'prefix' => 'magic-auth',
-    'middleware' => "{$throttleMiddleware}:10,10",
 ], function (\Illuminate\Routing\Router $router) use ($controller) {
-    $router->post('create', "$controller@postCreate");
+    
+    $throttleMiddleware = \Illuminate\Routing\Middleware\ThrottleRequests::class;
+    
+    $router->post('create', ['middleware' => "{$throttleMiddleware}:10,5", 'uses' => "$controller@postCreate"]);
     $router->get('login', "$controller@getLogin");
     $router->post('verify', "$controller@postVerify");
 });
